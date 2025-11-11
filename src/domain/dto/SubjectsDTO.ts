fbdf3c359671652afde3ef360728de4381e7d8b8
VAranda <viniciuzaranda@gmail.com>
2025-11-11 03:42:27 -0300

    feat: add subjects, topics, questions, and alternatives models to Prisma schema
    - Introduced new models: Subject, Topic, Question, and Alternative in the Prisma schema.
    - Updated the migration file to create corresponding tables in the database.
    - Created DTOs for subject creation and updates.
    - Defined a Subject entity class to represent the Subject model.
    - Implemented an interface for the Subject repository with methods for CRUD operations.

---
+++ src/domain/dto/SubjectsDTO.ts
@@ -0,0 +1,8 @@
+// src/domain/dto/subject.dto.ts
+export interface CreateSubjectDTO {
+  name: string;
+}
+
+export interface UpdateSubjectDTO {
+  name?: string;
+}
---
+++ src/domain/entities/Subjects.ts
@@ -0,0 +1,9 @@
+// src/domain/entities/subject.ts
+export class Subject {
+  id: string;
+  name: string;
+
+  // Relações (opcional na entidade pura)
+  // topics?: Topic[];
+  // questions?: Question[];
+}
